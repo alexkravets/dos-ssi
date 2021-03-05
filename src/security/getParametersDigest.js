21017e079e0ded075f4c51c3474fff4e44594cce
Oleksandr Kravets <a@kra.vc>
2021-03-05 23:05:21 +0200

    Update getParametersDigest function

--- src/security/getParametersDigest.js
+++ src/security/getParametersDigest.js
@@ -1,9 +1,22 @@
 'use strict'
 
 const canonicalize   = require('canonicalize')
+const { stringify }  = require('querystring')
 const { createHash } = require('crypto')
 
-const getParametersDigest = (parameters) => {
+const getParametersDigest = ({ mutation, ...query }) => {
+  const parameters = {}
+
+  /* istanbul ignore else */
+  if (query) {
+    parameters.query = stringify(query)
+  }
+
+  /* istanbul ignore else */
+  if (mutation) {
+    parameters.mutation = mutation
+  }
+
   const canonized = canonicalize(parameters)
   const digest = createHash('sha256').update(canonized).digest().toString('hex')
 
